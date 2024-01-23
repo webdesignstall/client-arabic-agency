4139886c077ba844d7c0980d85bb28f6cf4e415c
shakil <shakilhossen307@gmail.com>
2024-01-23 14:14:17 +0600

    create mobile navber dropdown menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,8 +73,8 @@ const config = {
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-down": "accordion-down 0.4s ease-out",
+        "accordion-up": "accordion-up 0.4s ease-out",
       },
     },
   },
