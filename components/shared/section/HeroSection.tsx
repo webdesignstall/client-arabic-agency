0b25a166ff866ed337cd2af77f274ba07a09e2c5
shakil <shakilhossen307@gmail.com>
2024-01-23 08:51:41 +0600

    sum bug fixed

--- components/shared/section/HeroSection.tsx
+++ components/shared/section/HeroSection.tsx
@@ -3,7 +3,6 @@ import React, {useEffect, useState} from 'react'
 import axios from "axios";
 import { translate } from '@/utility/translate';
 
-<<<<<<< HEAD
 // @ts-ignore
 export default function HeroSection({homeData}) {
 
@@ -33,16 +32,6 @@ export default function HeroSection({homeData}) {
                   </h1>
                 </div>
               </div>
-=======
-export default function HeroSection() {
-  return (
-    <div>
-      <div>
-        <div className='m-auto bg-cover h-[40vh] lg:h-[95vh] bg-[url("https://images.unsplash.com/photo-1704658492989-e4a4d1db44ba?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]'>
-          <div className='h-full flex w-full items-end bg-gradient-to-t from-[#020d1c] from-0.5%  via-transparent via-30% to-transparent to-90%'>
-            <div className='lg:py-8 py-5 lg:ml-28 max-w-7xl px-6'>
-              <h1 className='text-3xl lg:text-7xl text-white lg:font-bold'>AI Software Development</h1>
->>>>>>> e2dac81 (section responsive)
             </div>
           </div>
         </div>
--- components/shared/section/RatingSection.tsx
+++ components/shared/section/RatingSection.tsx
@@ -10,29 +10,20 @@ export default function RatingSection({homeData}) {
     <SectionContainer>
       <div>
         <div className=' grid grid-cols-12'>
-<<<<<<< HEAD
           <div className='text-xl font-light lg:text-[2.25rem] lg:font-semibold col-span-7 lg:col-span-8 leading-[3rem] max-w-3xl'>
             {translate(homeData, 'heroSectionDescription')}
-=======
-          <div className='text-xl font-light lg:text-[2.25rem] lg:font-semibold col-span-12  lg:col-span-8 lg:leading-[3rem] max-w-3xl'>
-            Elevate your success with efficient custom AI solutions sculpted by our expert team with vast expertise.
->>>>>>> e2dac81 (section responsive)
           </div>
           <div className='border-r'>
 
           </div>
-          <div className='pl-6 pt-4 lg:col-span-3 col-span-6 h-20 bg-cover bg-[url("https://static.andersenlab.com/andersenlab/new-andersensite/icons/logos/clutch-top-info-tech-white.svg")]'>
+          <div className='pl-6 lg:col-span-3 h-20 bg-cover bg-[url("https://static.andersenlab.com/andersenlab/new-andersensite/icons/logos/clutch-top-info-tech-white.svg")]'>
             <div className='flex justify-center items-center space-x-6'>
               <div className='brightness-50 shrink-0'>
                 <img className='w-24 h-24' src={`${process.env.NEXT_PUBLIC_BACKED_BASE}${ homeData?.clutchTopInfo?.data?.attributes?.url}`} alt="" />
               </div>
               <div className='space-y-2'>
-<<<<<<< HEAD
                 <img className='w-24 h-24' src={`${process.env.NEXT_PUBLIC_BACKED_BASE}${ homeData?.clutchReviewStar?.data?.attributes?.url}`} alt="" />
                 {/*<div className='uppercase font-semibold'>49 on clutch</div>
-=======
-                <div className='uppercase font-semibold text-sm lg:text:lg'>49 on clutch</div>
->>>>>>> e2dac81 (section responsive)
                 <div className='flex'>
                   <Star />
                   <Star />
@@ -40,11 +31,7 @@ export default function RatingSection({homeData}) {
                   <Star />
                   <Star />
                 </div>
-<<<<<<< HEAD
                 <p>100+ reviews</p>*/}
-=======
-                <p className='text-sm lg:text-lg'>100+ reviews</p>
->>>>>>> e2dac81 (section responsive)
               </div>
             </div>
           </div>
